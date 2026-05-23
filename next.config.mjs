5063f2726693e67e5ef0f420d2fe64da0adca63b
Ritwik Desai <77358231+ritwikdesai07@users.noreply.github.com>
2026-05-23 18:10:36 -0400

    Add files via upload
    first upload

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  onDemandEntries: {
+    maxInactiveAge: 60000,
+    pagesBufferLength: 5,
+  },
+}
+
+export default nextConfig
